d869808de0d9fa98480c402822a8f4d17b4808a3
anita <anita8004@gmail.com>
2024-01-22 17:20:36 +0800

    add store and counter

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import {useAppDispatch, useAppSelector} from '@/hooks';
+import {
+  decrement,
+  increment,
+  selectCount
+} from './counterSlice';
+import { Flex, Text, Button } from '@radix-ui/themes'
+
+export default function Counter() {
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+  return (
+    <Flex py="5" align="center" justify="center">
+      <Button 
+        radius="large" 
+        variant="soft"
+        onClick={() => dispatch(decrement())}
+      >Decrement</Button>
+      <Text as="span" mx="5">{ count }</Text>
+      <Button 
+        radius="large" 
+        variant="soft"
+        onClick={() => dispatch(increment())}
+      >Increment</Button>
+    </Flex>
+  )
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import {
 } from 'react-router-dom'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
+import Counter from '@/features/counter/Counter'
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: '/sign-up',
     element: <SignUp />
+  },
+  {
+    path: '/counter',
+    element: <Counter />
   }
 ])
 
